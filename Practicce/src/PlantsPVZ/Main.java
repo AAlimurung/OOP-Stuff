4ec5d7b34b5efd14c2231d111f87472a0e1b6038
reeaash <117620183+AAlimurung@users.noreply.github.com>
2023-10-11 21:57:58 +0800

    Update Main.java

--- Practicce/src/PlantsPVZ/Main.java
+++ Practicce/src/PlantsPVZ/Main.java
@@ -14,8 +14,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.print("Game Mode: ");
         String mode = sc.nextLine();
-
+        boolean state = mode.equals("Fog") || mode.equals("Night");
         String input;
+
         do {
             System.out.print("Add a plant: ");
             input = sc.nextLine();
@@ -26,7 +27,7 @@ public class Main {
                     plants.add(new Plant.WallNut());
                     break;
                 case "Sun-shroom":
-                    plants.add(new Mushroom.SunShroom(false));
+                    plants.add(new Mushroom.SunShroom(state));
                     break;
                 // add more plants here
                 case "Sunflower":
@@ -36,10 +37,10 @@ public class Main {
                     plants.add(new Plant.Peashooter());
                     break;
                 case "Puff-shroom":
-                    plants.add(new Mushroom.PuffShroom(false));
+                    plants.add(new Mushroom.PuffShroom(state));
                     break;
                 case "Doom-shroom":
-                    plants.add(new Mushroom.DoomShroom(false));
+                    plants.add(new Mushroom.DoomShroom(state));
                     break;
                 case "Jalapeno":
                     plants.add(new Plant.Jalapeno());
@@ -51,16 +52,31 @@ public class Main {
                     plants.add(new Plant.LilyPad());
                     break;
                 case "Cattail":
+                    for(Plant p : plants){
+                        if(p instanceof Plant.LilyPad){
+                            int pin = plants.indexOf(p);
+                            plants.add(pin, (Plant) ((Upgradable)p).upgrade());
+                            plants.remove(p);
+                            break;
+                        }
+                    }
+                    break;
+                case "Twin Sunflower":
+                    for (Plant p : plants){
+                        if(p instanceof Plant.Sunflower){
+                            int pin = plants.indexOf(p);
+                            plants.add(pin, (Plant)((Upgradable)p).upgrade());
+                            plants.remove(p);
+                            break;
+                        }
+                    }
                     break;
                 case "Coffee Bean":
                     Plant.CoffeeBean cb = new Plant.CoffeeBean();
                     for (Plant p : plants){
-                        if(p instanceof Mushroom){
-                            if(((Mushroom)p).isAwake()){
-                            }else {
-                                ((Mushroom)p).awaken(cb);
-                                break;
-                            }
+                        if(p instanceof Mushroom && !((Mushroom)p).isAwake()){
+                            ((Mushroom)p).awaken(cb);
+                            break;
                         }
                     }
                     break;
@@ -70,6 +86,8 @@ public class Main {
         } while (!input.equals("DONE"));
 
         do {
+            int counter = 0; //counter sun, total
+            int pop = 0; //counter sun producers, num
             System.out.print("Do something: ");
             input = sc.nextLine();
             switch (input) {
@@ -77,69 +95,81 @@ public class Main {
                     break;
                 case "Produce Sun":
                     // add implementation here
-                    int counterSun = 0;
-                    int counterSunProducer = 0;
                     for(Plant p : plants){
-                        if(p instanceof SunProducer){
-                            counterSun += ((SunProducer)p).produce_sun();
-                            counterSunProducer++;
-                            System.out.println(p.name + " produces " +  ((SunProducer)p).produce_sun() + " suns");
+                        if(p instanceof SunProducer && p.isAlive()){
+                            pop++;
+                            counter += ((SunProducer)p).produce_sun();
                         }
                     }
-                    System.out.println(counterSunProducer + " sun producers gather " + counterSun + " suns");
+                    System.out.println(pop + " sun producers gather " + counter + " suns");
                     break;
                 case "Attack":
                     // add implementation here
                     for(Plant p : plants){
-                            
+                        if(p instanceof Attacker && p.isAlive()){
+                            pop++;
+                            counter += ((Attacker)p).attack();
+                        }
+                    }
+                    if(pop == 0){
+                        System.out.println("You have no attackers");
+                    }else{
+                        System.out.println(pop + " attackers dealing " + counter + " damage");
                     }
                     break;
                 // add more cases here
                 case "Attacker Status":
                     for(Plant p : plants){
-                        if(p instanceof Attacker) {
-                            if (((Attacker) p).rangeType() == 0) {
-                                System.out.println(p.name + " can attack on a single line");
-                            }
-                            if (((Attacker) p).rangeType() == 1) {
-                                System.out.println(p.name + " can attack using area-of-effect");
-                            }
-                            if (((Attacker) p).rangeType() == -1) {
-                                System.out.println(p.name + " can attack only when enemy is nearby");
+                        if(p instanceof Attacker && p.isAlive()) {
+                            pop++;
+                            switch (((Attacker)p).rangeType()){
+                                case SINGLE_LINE:
+                                    System.out.println(p.name + " can attack on a single line");
+                                    break;
+                                case AOE:
+                                    System.out.println(p.name + " can attack using area-of-effect");
+                                    break;
+                                case LIMITED:
+                                    System.out.println(p.name + " can attack only when enemy is nearby");
+                                    break;
+                                case FREE_RANGE:
+                                    System.out.println(p.name + " can attack any enemies from anywhere");
+                                    break;
                             }
                         }
+                    }
+                    if(pop == 0){
+                        System.out.println("You have no attackers");
                     }
                     break;
                 case "Instant Kill Status":
                     for(Plant p : plants){
-                        if(p instanceof InstantKiller){
-                            if(((InstantKiller)p).killType() == 1){
-                                System.out.println(p.name + " can kill instantly");
-                            }
-                            if(((InstantKiller)p).killType() == 0){
-                                System.out.println(p.name + " can kill on contact");
+                        if(p instanceof InstantKiller && p.isAlive()){
+                            pop++;
+                            switch (((InstantKiller)p).killType()){
+                                case INSTANT:
+                                    System.out.println(p.name + " can kill instantly");
+                                    break;
+                                case CLOSE_ON:
+                                    System.out.println(p.name + " can kill on contact");
+                                    break;
                             }
                         }
                     }
+                    if(pop == 0){
+                        System.out.println("You have no plants which can kill instantly");
+                    }
                     break;
                 case "Sort by HP":
-                  plants.sort(new Plant.sortbyHP());
+                    plants.sort(new Plant.sortbyHP());
                     for(Plant p : plants){
-                        if(p.hp == INFINITE){
-                            System.out.println(p.name + " (∞) - cost: " + p.sun_cost);
-                        }else{
-                            System.out.println(p.name + " (" + p.hp + ") - cost: " + p.sun_cost);
-                        }
+                        System.out.println(p);
                     }
                     break;
                 case "Sort by Sun Cost":
                     plants.sort(new Plant.sortbySunCost());
                     for(Plant p : plants){
-                        if(p.hp == INFINITE){
-                            System.out.println(p.name + " (∞) - cost: " + p.sun_cost);
-                        }else{
-                            System.out.println(p.name + " (" + p.hp + ") - cost: " + p.sun_cost);
-                        }
+                        System.out.println(p);
                     }
                     break;
                 case "Sort by Name":
