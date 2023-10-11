05c57c6a815779bbebca4860be7dd6435bcadfec
reeaash <117620183+AAlimurung@users.noreply.github.com>
2023-10-11 22:03:59 +0800

    Update Settings.java

--- Practicce/src/PlantsPVZ/Settings.java
+++ Practicce/src/PlantsPVZ/Settings.java
@@ -4,21 +4,28 @@ interface SunProducer {
     int produce_sun();
 }
 
-
 interface PlantUpgrade {
     int concurrentSunCost();
 }
 
 interface Attacker {
     int attack(); //print attack action, return integer of dmg
-    int rangeType(); //return integer of range capability (SL / AOE)
+    RangeType rangeType(); //return integer of range capability (SL / AOE)
 }
 
 interface InstantKiller{
-    int killType();
+    KillType killType();
 }
 
 interface Upgradable{
-    String upgrade();
+    PlantUpgrade upgrade();
 }
 // add more interfaces here
+
+enum RangeType{
+    SINGLE_LINE, AOE, LIMITED, FREE_RANGE;
+}
+
+enum KillType{
+    INSTANT, CLOSE_ON;
+}
